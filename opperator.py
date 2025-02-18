278810a73e0a2ed5906a9a38d7650d88a9ee5ead
Asif Pansuvi <m.asifpansuvi2266@gmail.com>
2025-02-18 07:54:44 +0500

    Python for AI

---
+++ opperator.py
@@ -0,0 +1,12 @@
+# arthimetic operator
+# PEMDAS = paraenthesis exponential muktiply division addition subtraction
+
+num1 = int(input('enter num 1: '))
+num2 = int(input('enter num 2: '))
+
+print(f'num 1 + num 2: {num1 + num2}')
+print(f'num 1 - num 2: {num1 - num2}')
+print(f'num 1 * num 2: {num1 * num2}')
+print(f'num 1 / num 2: {num1 / num2}')
+print('Multiply of two num = {0} division of two num = {1} addition of two num = {2} subtraction of two num = {3}'.format(num1 / num2,num1 * num2,num1 + num2,num1 - num2))
+
